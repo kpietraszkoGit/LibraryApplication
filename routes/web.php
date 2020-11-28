fb1e7e70a372cc8fe05c43afe180dfbb98eec0fc
kpietraszkoGit <54677750+kpietraszkoGit@users.noreply.github.com>
2020-11-28 10:22:16 +0100

    Update web.php
    changed code

--- routes/web.php
+++ routes/web.php
@@ -14,33 +14,13 @@ use App\Http\Controllers\SitesController;
 |
 */
 
-//tutaj jest odnisienie do  naszej strony startowej Laravel naszego views->pliku welcome.blade.php
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-//Route::get('/sites', 'SitesController@index'); //wskazujemy na metode która ma sie wykonać podczas tej akcji czyli index
-//Strona glowna
 Route::get('/', [SitesController::class, 'index'])->name('sites.index');
 
 Route::get('/add', [SitesController::class, 'add'])->name('sites.add');
 
-//post bo formuarz będzie wywoływana metoda post
-// Route::post('/save', [
-//     'uses' => 'SitesController@save',
-//     'as' => 'sites.save' //ustalanie URL
-// ]);
-
-// Route::post('/save', [
-//          'uses' => [SitesController::class, 'save'],
-//          'as' => 'sites.save' //ustalanie URL
-//      ]);
-
 Route::post('/save', [SitesController::class, 'save'])->name('sites.save');
 
-//Route::post('/sites/{site}', [SitesController::class, 'save'])->name('sites.save');
-
-Route::get('/site/{site}', [SitesController::class, 'show'])->name('sites.show');//{site}jest to parametr w adresie URL
+Route::get('/site/{site}', [SitesController::class, 'show'])->name('sites.show');
 
 Route::get('/sites/edit/{site}', [SitesController::class, 'edit'])->name('sites.edit');
 
