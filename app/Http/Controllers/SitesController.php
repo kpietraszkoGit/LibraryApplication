329b483dd9efe5ee308c5f154c1a359e274bf753
kpietraszkoGit <54677750+kpietraszkoGit@users.noreply.github.com>
2020-11-28 10:10:57 +0100

    Update SitesController.php
    add new function

--- app/Http/Controllers/SitesController.php
+++ app/Http/Controllers/SitesController.php
@@ -9,57 +9,42 @@ use Illuminate\Http\Request;
 
 class SitesController extends Controller
 {
-    //zabezpieczenie kontrolera zeby tylko użytkownik zalogowany mógł coś zrobić
     public function __construct()
     {
-        $this->middleware('auth');//sprawdza czy jestesmy zalogowani jeśli nie to nas wyrzuci to forumlarza logowania
+        $this->middleware('auth');
     }
 
     public function index()
     {
-        //pobieranie z bazy danej rekordy
-        //$sites = Site::all();//pobieranie danych z bazy do zmiennej sites
-
-        $sites = Site::orderBy('id','DESC')->simplePaginate(10);//odwrócenie kolejności zwaracania danych z bazy danej(najnowesze wpisy na samej górze) i wyświetlanie tylko 6 rekordów na stronie
-        return view('sites.index', compact('sites'));//2 sposób 'sites.index'-jest to szablon na którym pojawia się dane z bazy
-            //['sites' => $sites//1 sposób
-        //]);
+        $sites = Site::orderBy('id','DESC')->simplePaginate(10);
+        return view('sites.index', compact('sites'));
     }
 
-    //tworzenie tabeli i bedziemy cos do niej dodawac
     public function add()
     {
         return view('sites.add');
     }
 
-    //tworzenie nowej akcji dla formularza, pobieranie danych z formularza i wysylanie je do bazy danej
-    //public function save(Request $request)
     public function save(PagesRequest $request)
     {
-        //dd($request->all());
-        //Metoda 1
-        //Site::create($request->all());//1 metoda przesylu danych z formularza do bazy danej
-
-        //Metoda 2
         $site = new Site();
         $site->title = $request->input('title');
         $site->description = $request->input('description');
         $site->save();
         return view('save');
-        //return redirect()->route('sites.save');//to jest dodane, 1 metoda
     }
 
-    public function show(Site $site)//typujemy
+    public function show(Site $site)
     {
         return view('sites.show', compact('site'));
     }
 
-    public function edit(Site $site)//typujemy
+    public function edit(Site $site)
     {
         return view('sites.edit', compact('site'));
     }
 
-    public function update(PagesRequest $request, Site $site) //lub Sites
+    public function update(PagesRequest $request, Site $site)
     {
         $site->title = $request->input('title');
         $site->description = $request->input('description');
@@ -67,10 +52,9 @@ class SitesController extends Controller
         return view('sites.update', compact('site'));
     }
 
-    public function delete(Site $site) //lub Sites
+    public function delete(Site $site)
     {
         $site->delete();
-        //return redirect()->route('sites.index');
         return view('sites.delete', compact('site'));
     }
 }
